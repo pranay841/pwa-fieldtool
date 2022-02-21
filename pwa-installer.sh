dcfc400c57302f0017052fc53514db9bae0a58e4
pranayhbwsl <pranay.chahare@hbwsl.com>
2022-02-21 17:11:06 +0530

    test

--- pwa-installer.sh
+++ pwa-installer.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-DIR="/workspace/pwa-deals/@hbwsl"
+DIR="/workspace/pwd-fieldtool/@humcommerce "
 if [ ! -d "$DIR" ]; then
     #removing module files from root
     folders=(.github src)
@@ -19,7 +19,7 @@ if [ ! -d "$DIR" ]; then
 rm -rf /workspace/magento2gitpod/node_modules
 rm -rf /workspace/magento2gitpod/.npm
 
-export NVM_DIR=/workspace/pwa-deals/nvm
+export NVM_DIR=/workspace/pwd-fieldtool/nvm
 mkdir -p $NVM_DIR
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.38.0/install.sh | bash
 . "$NVM_DIR/nvm.sh"
@@ -28,27 +28,27 @@ nvm use 16.14.0
 npm install yarn -g
 npm install rimraf -g
 
-cd /workspace/pwa-fieldtool && sudo apt update && sudo apt -y install expect
+cd /workspace/pwd-fieldtool && sudo apt update && sudo apt -y install expect
 
-chmod a+rwx /workspace/pwa-fieldtool/initialize-theme.sh
-chmod a+rwx /workspace/pwa-fieldtool/install-theme.exp &&
-/workspace/pwa-fieldtool/install-theme.exp
+chmod a+rwx /workspace/pwd-fieldtool/initialize-theme.sh
+chmod a+rwx /workspace/pwd-fieldtool/install-theme.exp &&
+/workspace/pwd-fieldtool/install-theme.exp
 
-cd /workspace/pwa-fieldtool/fieldtooldev && cp -avr .* /workspace/pwa-fieldtool;
-cd /workspace/pwa-fieldtool && rm -r -f fieldtooldev;
-cd /workspace/pwa-fieldtool
+cd /workspace/pwd-fieldtool/fieldtooldev && cp -avr .* /workspace/pwd-fieldtool;
+cd /workspace/pwd-fieldtool && rm -r -f fieldtooldev;
+cd /workspace/pwd-fieldtool
 
-mkdir /workspace/pwa-fieldtool/@humcommerce && cd /workspace/pwa-fieldtool/@humcommerce
+mkdir /workspace/pwd-fieldtool/@humcommerce && cd /workspace/pwd-fieldtool/@humcommerce
 
 ORIGIN_VALUE=$(git config --get remote.origin.url)
 
-git clone $ORIGIN_VALUE && mv pwa-fieldtool fieldtool
-cd /workspace/pwa-fieldtool && rm -rf .git
-sed -i 's/_SERVER_PORT=0/_SERVER_PORT=10000/g' /workspace/pwa-fieldtool/.env
+git clone $ORIGIN_VALUE && mv pwd-fieldtool fieldtool
+cd /workspace/pwd-fieldtool && rm -rf .git
+sed -i 's/_SERVER_PORT=0/_SERVER_PORT=10000/g' /workspace/pwd-fieldtool/.env
 
 yarn add sass sass-loader@^10.1.1 link:./@humcommerce/fieldtool
 
 WEBPACK_CONFIG_EXT="config.module.rules.push({test: /\\.s[ca]ss$/,use: ['style-loader',{loader: require.resolve('css-loader'),options: {importLoaders: 1,modules: true,modules : {localIdentName: '[name]__[local]__[hash:base64:3]',},},},'sass-loader']});"
-sed  -i".bak" -e  "/return \[config\]/i $WEBPACK_CONFIG_EXT" /workspace/pwa-fieldtool/webpack.config.js
+sed  -i".bak" -e  "/return \[config\]/i $WEBPACK_CONFIG_EXT" /workspace/pwd-fieldtool/webpack.config.js
 yarn install && yarn run build && yarn run start
 fi
